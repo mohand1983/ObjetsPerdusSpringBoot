1c5bcad15fe5e144ed318336b1bd5878a0b9eb21
boudi1983 <boudi.mohand@gmail.com>
2024-01-03 13:47:26 +0100

    initial commit

---
+++ src/test/java/fr/ObjetsPerdus/ObjetsPerdus/ObjetsPerdusApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ObjetsPerdus.ObjetsPerdus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetsPerdusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
